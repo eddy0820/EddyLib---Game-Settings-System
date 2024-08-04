78fc3e826e331b7c7d5d4156530617733ee684da
eddy0820 <59713264+eddy0820@users.noreply.github.com>
2024-08-04 15:55:19 -0400

    updated oopsy again

---
+++ GameSettingsSystemSettingsProvider.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace EddyLib.GameSettingsSystem.Editor
+{
+
+public class GameSettingsSystemSettingsProvider : SettingsProvider
+{
+    SerializedObject gameSettingsSystemSettings;
+
+    const string SETTINGS_ASSETS_PATH = "Assets/Resources/EddyLib/EddyLib.GameSettingsSystem.asset";
+
+    public GameSettingsSystemSettingsProvider(string path, SettingsScope scope = SettingsScope.Project) : base(path, scope) {}
+
+    public static bool IsSettingsAvailable()
+    {
+        return File.Exists(SETTINGS_ASSETS_PATH);
+    }
+
+    public override void OnActivate(string searchContext, VisualElement rootElement)
+    {
+        gameSettingsSystemSettings = GameSettingsSystemSettings.GetSerializedSettings();
+    }
+
+    public override void OnGUI(string searchContext)
+    {
+        EditorGUILayout.PropertyField(gameSettingsSystemSettings.FindProperty("defaultGameSettingsPath"));
+        gameSettingsSystemSettings.ApplyModifiedPropertiesWithoutUndo();
+    }
+
+    [SettingsProvider]
+    public static SettingsProvider CreateGameSettingsSystemSettingsProvider()
+    {
+        var provider = new GameSettingsSystemSettingsProvider("Project/EddyLib/Game Settings System", SettingsScope.Project);
+        return provider;
+    }
+}
+
+}
