25b5f33bef769c28a4083665760e971ab0371246
eddy0820 <59713264+eddy0820@users.noreply.github.com>
2024-08-04 21:57:07 -0400

    updated scene settings

--- Scripts/SceneSettings.cs
+++ Scripts/SceneSettings.cs
@@ -8,6 +8,19 @@ public class SceneSettings : MonoBehaviour
 {
     [Expandable, SerializeField] GameSettingsSO defaultGameplaySettings;
 
+    private void Awake()
+    {
+        SceneSettings[] sceneSettings = FindObjectsOfType<SceneSettings>();
+
+        if(sceneSettings.Length > 1)
+        {
+            Debug.LogWarning("Multiple SceneSettings found in scene. Only one should exist.");
+            
+            for(int i = 1; i < sceneSettings.Length; i++)
+                Destroy(sceneSettings[i]);
+        }
+    }
+
     public T GetSettings<T>() where T : SettingsCategory
     {
         return defaultGameplaySettings.GetSettings<T>();
